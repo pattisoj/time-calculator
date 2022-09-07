48b43d944264037dcce44f7ab1f597db4a97a902
pattisoj <joshpattison23@icloud.com>
2022-09-07 10:07:48 +0100

    0/12 tests - Calculated and formatted result time

--- time_calculator.py
+++ time_calculator.py
@@ -19,6 +19,32 @@ def add_time(start, duration, day=False):
       total_hours = total_hours + int(total_mins / 60)
       total_mins = int(total_mins % 60)
 
-    print("Total hours:", total_hours)
-    print("Total mins:", total_mins)
-    return "Testing"
+  # If there was a duration - calculate the passage of time
+    days_later = 0
+    if day_time == "pm" and total_hours > 12:
+      # Changing to another day if start time is pm
+      if total_hours % 24 >= 1.0:
+        days_later += 1  
+              
+      if total_hours >= 12:
+        hours_left = total_hours / 24
+        days_later = days_later + int(hours_left)
+
+      total_hours_storage = total_hours
+      while True:
+        if total_hours_storage < 12:
+          break
+        if total_hours_storage >= 12:
+          if day_time == "am":
+            day_time = "pm"
+          elif day_time == "pm":
+            day_time = "am"
+        total_hours_storage = total_hours_storage - 12
+
+    remaining_hours = int(total_hours % 12) or hours + 1
+    remaining_mins = int(total_mins % 60)
+
+    # Format the resulting time 
+    result = f'{remaining_hours}:{remaining_mins:02} {day_time.upper()}'
+
+    print(result)
